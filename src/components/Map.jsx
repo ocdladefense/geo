a50c715847b72d81c57eee71de9b09be1e6e4311
José Bernal <jbernal.web.dev@gmail.com>
2026-03-19 09:48:47 -0700

    Add other components back in.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -59,7 +59,35 @@ export default function Map() {
         <>
             <div id="map" className="wrapper" style={{ width: "100svw", height: '120svh', position: "absolute", top: "0px", left: "0px" }}> </div>
 
+            <div className="relative h-[100vh] w-[100%] overflow-auto tablet:overflow-hidden">
 
+                <div className="static tablet:absolute tablet:w-[25%] top-[40px] left-[10px] z-10">
+
+                    <div id="form-container" className="block absolute top-[0px] left-[0px] w-auto tablet:static tablet:w-[120%]" style={{ backgroundColor: "rgba(255,255,255,0.9)", zIndex: "1", padding: '20px', boxSizing: 'border-box', margin: '10px', marginTop: '20px', borderRadius: '15px' }}>
+                        <AddressSearch mapManager={mapManager} onSubmit={submitFunction} />
+                    </div>
+
+
+                    <div className="hidden tablet:block w-[100%] " style={{ backgroundColor: "rgba(255,255,255,0.9)", zIndex: "1", padding: '20px', boxSizing: 'border-box', margin: '10px', marginTop: '20px', borderRadius: '5px' }}>
+                        <label for="district-select">Features</label><br />
+                        <select id="district-select" className="mb-4">
+                            <option value="">--Select feature--</option>
+                            <option value="house">House Districts</option>
+                            <option value="senate">Senate Districts</option>
+                        </select>
+
+
+                        <button onClick={() => mapManager.resetZoom()} style={{ backgroundColor: "#ccc", borderRadius: "3px", padding: '10px', fontSize: 'larger', marginTop: '20px' }} id="find-district" type="button">Reset zoom</button>
+                    </div>
+
+                </div>
+
+                {addresses.length > 0 &&
+                    <div className="block absolute bottom-0 left-0 overflow-y-scroll overflow-x-visible min-h-auto w-[100%] tablet:min-h-[200px] tablet:max-h-[200px] tablet:w-[750px] tablet:absolute tablet:bottom-0" style={{ backgroundColor: "rgba(255,255,255,0.9)", zIndex: "1", padding: '20px', boxSizing: 'border-box', margin: '10px', marginTop: "20px", borderRadius: '5px' }}>
+                        <Results addresses={addresses} onClick={handleResultClick} groupByField="none" />
+                    </div>}
+
+            </div>
         </>
     );
 }
