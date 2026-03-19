676d63c49373c549be6d2c46e1adefd918744dde
José Bernal <jbernal.web.dev@gmail.com>
2026-03-19 09:01:24 -0700

    Use vh,vw units for outer divs.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -55,7 +55,7 @@ export default function Map() {
 
     return (
         <>
-            <div id="map" style={{ width: '100%', height: '100%', position: "fixed", top: 0, left: 0 }}></div>
+            <div id="map" style={{ width: "100vw", height: '100vh', position: "fixed", top: 0, left: 0 }}></div>
 
             <div className="relative h-[100vh] w-[100%] overflow-auto tablet:overflow-hidden">
 
